6b88ba3b86bf28a58ec62b2d1bd11821f5e2b3ef
jonhtheone <jpssouza7@gmail.com>
2025-02-22 11:44:48 -0300

    altered package.path in init.lua altered in order to turn the metadata file into a module

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-package.path = ";/home/jonhson/.config/nvim/lua/?/.lua;/home/jonhson/.config/nvim/lua/plugins/?.lua;/home/jonhson/.config/nvim/globals/?.lua;/home/jonhson/.config/nvim/globals/funcs/?.lua;"
+package.path = ";/home/jonhson/.config/nvim/lua/?/.lua;/home/jonhson/.config/nvim/lua/plugins/?.lua;/home/jonhson/.config/nvim/globals/?.lua;/home/jonhson/.config/nvim/globals/funcs/?.lua;/home/jonhson/.config/nvim/globals/funcs/?/init.lua"
 require("globals")
 require("remaps")
 require("autocmds")
