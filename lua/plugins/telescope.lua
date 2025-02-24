ba1bb98cd20854d25802564963bfba2d30ec0dad
jonhtheone <jpssouza7@gmail.com>
2025-02-24 11:09:12 -0300

    made another change in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
         vim.keymap.set("n", "<Leader>ff", function() builtin.find_files({hidden=true}) end, { desc = "find files in curr dir" } )
         vim.keymap.set("n", "<Leader>fb", builtin.current_buffer_fuzzy_find, { desc = "fuzzy find buffer" })
         vim.keymap.set("n", "<Leader>fc", function() builtin.live_grep({additional_args=function() return {"--hidden"} end}) end, { desc = "live grep in dir" })
-        vim.keymap.set("n", "<Leader>fg", builtin.buffers, {desc="search for list of buffers"})
+        vim.keymap.set("n", "<Leader>fg", function() builtin.buffers() end, {desc="search for list of buffers"})
         vim.keymap.set("n", "<Leader>fw", function() builtin.find_files({ cwd = vim.g.wiki_root, hidden=true}) end, { desc = "search D:/wikis/wiki" })
     end
 }
