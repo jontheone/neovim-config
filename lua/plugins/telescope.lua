710278fa0e571ae892d3758dfac19b036478e0c3
jonhtheone <jpssouza7@gmail.com>
2025-03-06 14:35:30 -0300

    tirei os seguintes plugins: whichkey, todofolke, snacks, markdowntoc

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,6 @@ return {
     config = function(_, opts)
         local builtin = require("telescope.builtin")
         require("telescope").setup(opts)
-        require("telescope").load_extension("todo-comments")
         vim.keymap.set("n", "<Leader>ff", function() builtin.find_files({hidden=true}) end, { desc = "find files in curr dir" } )
         vim.keymap.set("n", "<Leader>fb", builtin.current_buffer_fuzzy_find, { desc = "fuzzy find buffer" })
         vim.keymap.set("n", "<Leader>fc", function() builtin.live_grep({additional_args=function() return {"--hidden"} end}) end, { desc = "live grep in dir" })
