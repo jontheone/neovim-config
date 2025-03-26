24f3cce0db1974548432a51c0452f6f94d7b4cb3
jonhson <jpssouza7@gmail.com>
2025-03-26 00:27:26 -0300

    colorscheme config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,30 +6,35 @@ return {
             options = {
                 theme = "auto",
                 icons_enable = true,
-                section_separators = { left = "", right = "" },
-                component_separators = { left = '/', right = '/' }
+                section_separators = { left = "", right = "" },
+                component_separators = { left = '│', right = '│' }
             },
             sections = {
                 lualine_a = {"mode"},
                 lualine_b = {"filename", "filetype"},
-                lualine_c = {},
+                lualine_c = {
+                    {
+                        'buffers',
+                        mode = 1
+                    }
+                },
                 lualine_x = {},
                 lualine_y = {"progress", "location"},
                 lualine_z = {"os.date('%d-%m-%y')"}
             },
-            tabline = {
-                lualine_a = {
-                    {
-                        'buffers',
-                        mode = 2
-                    }
-                },
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {"tabs"}
-            }
+--            tabline = {
+--                lualine_a = {
+--                    {
+--                        'buffers',
+--                        mode = 1
+--                    }
+--                },
+--                lualine_b = {},
+--                lualine_c = {},
+--                lualine_x = {},
+--                lualine_y = {},
+--                lualine_z = {"tabs"}
+--            }
         }
     end
 }
