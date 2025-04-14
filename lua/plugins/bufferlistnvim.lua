f36824edbcea2042b544a8c1a4a7cea6be7b3113
jonhson <jpssouza7@gmail.com>
2025-04-14 13:24:11 -0300

    added a code formatter and removed noice for some time

--- lua/plugins/bufferlistnvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "EL-MASTOR/bufferlist.nvim",
-  lazy = true,
-  dependencies = "nvim-tree/nvim-web-devicons",
-  cmd = "BufferList",
-  opts = {}
-}
--- lua/plugins/colorpickernvim.lua
+++ lua/plugins/colorpickernvim.lua
@@ -1,12 +1,13 @@
 return {
     'uga-rosa/ccc.nvim',
     config = function()
+        local mappings = require("ccc").mapping
         require("ccc").setup({
             lsp = false,
             highlighter = {
                 auto_enable = false,
                 lsp = false
-            }
+            },
         })
         vim.keymap.set("n", "<leader>pc", "<cmd>CccPick<CR>", {desc="color picker"})
     end
