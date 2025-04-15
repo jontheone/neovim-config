f0e58f1b56a2dedd08f166f0faa014e4e54d52e6
jonhson <jpssouza7@gmail.com>
2025-04-15 20:47:39 -0300

    mudei o esquema de cores

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -33,45 +33,45 @@ return {
                 --            vim.cmd("colorscheme everforest")
             end,
 
-        },
-        {
-            'shaunsingh/nord.nvim',
-            lazy = false,
-            priority = 1000,
-            config = function()
-                --vim.cmd("colorscheme nord")
-                vim.g.nord_contrast = true
-                vim.g.nord_borders = false
-                vim.g.nord_disable_background = false
-                vim.g.nord_italic = false
-                vim.g.nord_uniform_diff_background = true
-                vim.g.nord_bold = false
-            end
-        },
-        {
-            'xiyaowong/transparent.nvim',
-            config = function()
-                --require("transparent").clear_prefix("lualine")
-            end
-        },
-        {
-            'nyoom-engineering/oxocarbon.nvim',
-            config = function()
-                --vim.cmd("colorscheme oxocarbon")
-            end
-        },
-        {
-            'widatama/vim-phoenix',
-            config = function()
-                --vim.cmd.colorscheme("phoenix")
-            end
-        },
-        {
-            "projekt0n/github-nvim-theme",
-            priority = 1000,
-            lazy = false,
-            config = function()
-                vim.cmd('colorscheme  github_dark_colorblind')
-            end
-        }
+    },
+    {
+        'shaunsingh/nord.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            --vim.cmd("colorscheme nord")
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = false
+            vim.g.nord_disable_background = false
+            vim.g.nord_italic = false
+            vim.g.nord_uniform_diff_background = true
+            vim.g.nord_bold = false
+        end
+    },
+    {
+        'xiyaowong/transparent.nvim',
+        config = function()
+            --require("transparent").clear_prefix("lualine")
+        end
+    },
+    {
+        'nyoom-engineering/oxocarbon.nvim',
+        config = function()
+            --vim.cmd("colorscheme oxocarbon")
+        end
+    },
+    {
+        'widatama/vim-phoenix',
+        config = function()
+            --vim.cmd.colorscheme("phoenix")
+        end
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            vim.cmd('colorscheme  github_dark_colorblind')
+        end
     }
+}
