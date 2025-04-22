81797dea29071e88f2f110c6793c1d2cd2476545
jonhson <jpssouza7@gmail.com>
2025-04-22 16:08:17 -0300

    Finalizei a modificação do arquivo init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-package.path = ";C:/users/jpsso/appdata/local/nvim/lua/globals/funcs/?.lua;C:/users/jpsso/appdata/local/nvim/lua/globals/funcs/?/init.lua"
+package.path = package.path..";C:/Users/jpsso/appdata/local/nvim/lua/globals/funcs/?.lua;C:/Users/jpsso/appdata/local/nvim/lua/globals/funcs/?/init.lua"
 require("globals.globals")
 require("globals.termhighlights")
 require("globals.remaps")
