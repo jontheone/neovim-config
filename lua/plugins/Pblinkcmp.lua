e73f4a9da9f243b7c5be882cd2ce741411bdd7ef
jontheone <jpssouza7@gmail.com>
2025-08-17 23:59:33 -0300

    made some alterations in blinks cmp mampping

--- lua/plugins/Pblinkcmp.lua
+++ lua/plugins/Pblinkcmp.lua
@@ -7,13 +7,13 @@ return {
         enabled = function() return true end,
         keymap = {
             preset = 'default',
-            ["<C-n>"] = { 'snippet_forward', 'fallback' },
-            ["<C-p>"] = { 'snippet_backward', 'fallback' },
-            ["<C-j>"] = { 'select_next', 'fallback' },
-            ["<C-k>"] = { 'select_prev', 'fallback' },
+            ["<C-l>"] = { 'snippet_forward', 'fallback' },
+            ["<C-h>"] = { 'snippet_backward', 'fallback' },
+            -- ["<C-j>"] = { 'select_next', 'fallback' },
+            -- ["<C-k>"] = { 'select_prev', 'fallback' },
         },
         appearance = { nerd_font_variant = 'mono' },
-        completion = { documentation = { auto_show = false } },
+        completion = { documentation = { auto_show = false }, menu = {ghost_text = { enabled = true } } },
         sources = { default = { 'lsp', 'path', 'snippets', 'buffer' }, }
     }
 }
